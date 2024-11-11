435dedd2dd5ba356b2afbe92e14b95183dece517
Yejin :P <78716519+YeJinPaark@users.noreply.github.com>
2024-11-11 17:40:34 +0900

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -9,7 +9,7 @@ class egemaps_dataset(Dataset):
     def __init__(self, df, pad_len = 94):
         self.fpath = df['path']    # File paths for each sample
         self.label = df['asd']     # Labels for each sample
-        self.pad_len = pad_len
+        self.pad_len = pad_len     # Padding length for feature sequences
 
     def __len__(self):
         return len(self.label)
