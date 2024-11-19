1917b74ffa1b1f2cddeeb5db655efe8f7780bed5
Seunghun Jeong <33884931+zldzmfoq12@users.noreply.github.com>
2024-11-19 11:02:53 +0900

    Update test_wav2vec_2023.py

--- test_wav2vec_2023.py
+++ test_wav2vec_2023.py
@@ -41,9 +41,6 @@ testloader = torch.utils.data.DataLoader(
         collate_fn = collate_fn2,
     )
 
-
-# change path to checkpoint
-
 model = MMCATextModel()
 model.eval()
 
@@ -52,7 +49,6 @@ preds = []
 
 for x, label in tqdm(testloader):
     model.to("cuda")
-    # y_hat_l, _, _, _, _, _, _, _ = model((x[0].to("cuda"), x[1], x[2].to("cuda")))
     probs = model((x[0].to("cuda"), x[1]))[0]
     
     # probs = F.sigmoid(y_hat_l[0][0])
